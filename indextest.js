886fcc6168ed78e1a8d6141617fea26445a449e7
whitevbear <whitevbear@gmail.com>
2023-12-07 17:55:01 +0800

    add try again button

--- indextest.js
+++ indextest.js
@@ -3,8 +3,9 @@
     console.log("answer: " + targetNumber);
 
     // Get element
-    const guessField = document.getElementById("guessField");
+    const guessText = document.getElementById("guessText");
     const guessSubmit = document.getElementById("guessSubmit");
+    const answerButton = document.getElementById("answerButton");
     const message = document.querySelector(".message");
     const theAnswer = document.querySelector(".theanswer");
 
@@ -15,44 +16,48 @@
     
 
     let timeCount = 0;
-    let img = document.createElement('img');
-    img.src ="meme01.jpg";
-
-
+    let meme01 = document.createElement('img');
+    meme01.src ="meme01.jpg";
+    // tryAgain 
+    let tryAgain = document.createElement('button');
+    tryAgain.textContent = "再玩一次";
+    tryAgain.className = "tryAgainbutton"
+ 
     // Event listener
     guessSubmit.addEventListener("click", checkGuess);  
-    answerbutton.addEventListener("click", askAnswer);
+    answerButton.addEventListener("click", askAnswer);
     
     // Answer function
     function askAnswer(){
         theAnswer.textContent = targetNumber;
     }
 
-   
-
-
-
 
     // Guess function
       function checkGuess() {
-      const userGuess = parseInt(guessField.value);
+      const userGuess = parseInt(guessText.value);
 
       timeCount = timeCount+1;//計數 也可寫成 timeCount++;
 
       console.log("userGuess: " + userGuess);
       console.log("timeCount: " + timeCount);
 
-        if ( timeCount>5 ) {
+        if ( timeCount>1 ) {
         // You Failed!
-        message.textContent = "You Failed! 答案是"+ targetNumber;
+        message.textContent = "You Failed! ";
         guessSubmit.disabled = true
+        theAnswer.textContent = "答案是"+targetNumber;
+        answerButton.disabled = true;
+        // tryAgain 
+        document.body.appendChild(tryAgain);
+        guessText.type = 'submit';
 
       } else if (userGuess === targetNumber) {
         message.textContent = "恭喜你，你猜對了！你這次猜了 "+timeCount+" 次哦~";
         message.style.color = "green";
         guessSubmit.disabled = true;
-        document.body.appendChild(img);
-        img.style.width = "200px";
+        document.body.appendChild(meme01);
+        meme01.style.width = "200px";
       } else if (userGuess < targetNumber) {
         lowerLimit=userGuess;
         message.textContent = userGuess + "到" +upperLimit+ "之間的數字，你猜是多少？";
@@ -67,6 +72,6 @@
         message.style.color = "red";
       }
 
-      guessField.value = "";
+      guessText.value = "";
       
     }
